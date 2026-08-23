938adc0fe6df431e70bf40cc21f3a42572a0c1ca
CodeCraftAbhi <133471829+Abhishekyadav012@users.noreply.github.com>
2026-08-24 00:20:34 +0530

    Change API base URL to production server

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-interview-system-backend-m0hx.onrender.com",
     withCredentials: true,
 })
 
